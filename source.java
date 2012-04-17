240349d8b72488c12f9611010ee375967d275db7
Ed Bartley <ed.bartley@gmail.com>
2012-04-17 09:29:39 -0300

    Update code and usage for AndEngine GLES2.0 branch

--- source.java
+++ source.java
@@ -1,38 +1,45 @@
-// Texture Source that takes a Bitmap
+import org.andengine.opengl.texture.atlas.bitmap.source.IBitmapTextureAtlasSource;
+import org.andengine.opengl.texture.atlas.source.BaseTextureAtlasSource;
+
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+
 public class BitmapTextureAtlasSource extends BaseTextureAtlasSource implements IBitmapTextureAtlasSource 
 {
-    private Bitmap mBitmap;
+    private final int[] mColors;
  
-    public BitmapTextureAtlasSource(Bitmap pBitmap) {
-    	super(0,0);
-        this.mBitmap = pBitmap.copy(Bitmap.Config.ARGB_8888, false);
+    public BitmapTextureAtlasSource(Bitmap pBitmap)
+    {
+    	super(0,0, pBitmap.getWidth(), pBitmap.getHeight());
+        
+        mColors = new int[mTextureHeight * mTextureWidth];
+        
+        for(int y = 0; y < mTextureHeight - 1; ++y)
+        {
+        	for( int x = 0; x < mTextureWidth - 1; ++x)
+        	{
+        		mColors[x + y * mTextureHeight] = pBitmap.getPixel(x, y);
+        	}
+        }
     }
- 
-    @Override
-    public int getWidth() {
-        return mBitmap.getWidth();
-    }
- 
-    @Override
-    public int getHeight() {
-        return mBitmap.getHeight();
-    }
- 
-    @Override
-    public BitmapTextureAtlasSource clone() {
-        return new BitmapTextureAtlasSource(Bitmap.createBitmap(mBitmap));
-    }
- 
+
 	@Override
 	public Bitmap onLoadBitmap(Config pBitmapConfig)
 	{
-		return mBitmap;
+		return Bitmap.createBitmap(mColors, mTextureWidth, mTextureHeight, Bitmap.Config.ARGB_8888);
+	}
+
+	@Override
+	public IBitmapTextureAtlasSource deepCopy()
+	{
+		return new BitmapTextureAtlasSource(Bitmap.createBitmap(mColors, mTextureWidth, mTextureHeight, Bitmap.Config.ARGB_8888));
 	}
 }
 
+
 // Usage:
 BitmapTextureAtlasSource source = new BitmapTextureAtlasSource(pBitmap);
-BitmapTextureAtlas texture = new BitmapTextureAtlas(mTextureSizeX, mTextureSizeY);
+BitmapTextureAtlas texture = new BitmapTextureAtlas(mContext.getTextureManager(), mTextureSizeX, mTextureSizeY);
 texture.addTextureAtlasSource(source, 0, 0);
-mEngine.getTextureManager().loadTexture(texture);
-TextureRegion region = new TextureRegion(texture, 0, 0, texture.getWidth(), texture.getHeight());
+texture.load();
+TextureRegion textureRegion = (TextureRegion) TextureRegionFactory.createFromSource(texture, source, 0, 0);
